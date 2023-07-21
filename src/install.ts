a90597ab0cc435bafd140fa2b0697539ff55eda8
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-21 18:40:29 +0600

    make install function sync

--- src/install.ts
+++ src/install.ts
@@ -1,19 +1,15 @@
-import {execa} from 'execa';
+import {execa, execaSync} from 'execa';
 import path from 'path';
 import fsExtra from 'fs-extra';
 
-export async function install({ directoryName }: InstallParam) {
-   if (await fsExtra.pathExists(directoryName)) {
+export function install({ directoryName }: InstallParam) {
+   if (fsExtra.pathExistsSync(directoryName)) {
       throw new Error(`The directory ${directoryName} already exists`);
     }
-  const gitInitResult = await execa(
+  execaSync(
     `git`, [`clone`, `https://github.com/deep-foundation/typescript-npm-package-template.git`, directoryName]
   );
-  if (gitInitResult.exitCode !== 0) {
-    throw new Error(gitInitResult.stderr);
-  }
-  console.log(gitInitResult.stdout);
-  await fsExtra.remove(path.join(directoryName, '.git'));
+  fsExtra.removeSync(path.join(directoryName, '.git'));
 }
 
 export interface InstallParam {
