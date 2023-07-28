fb97933f588925c7b3db14caf3710772e2903eda
FreePhoenix888 <freephoenix888@gmail.com>
2023-07-28 12:34:45 +0600

    Update automation and scripts

---
+++ automation/documentation/generate.ts
@@ -0,0 +1,28 @@
+import { glob } from 'glob';
+import {generateDocumentation} from '@deep-foundation/npm-automation'
+import path from 'path';
+import fsExtra from 'fs-extra';
+import { execa } from 'execa';
+
+main();
+
+async function main() {
+  const cliAppFilePaths = await glob(`./dist/cli/*.js`, {absolute: true})
+  for (const cliAppFilePath of cliAppFilePaths) {
+    fsExtra.chmodSync(cliAppFilePath, '755');
+  }
+  await generateDocumentation({
+    generateCliAppsHelpInReadmeOptions: {
+      cliAppFilePaths: cliAppFilePaths,
+      rootHeaderLevel: 2
+    },
+    generateUsageWaysOfNpmCliAppsInMarkdownFormatOptions: {
+      cliUtilityNames: cliAppFilePaths.map(cliAppFilePath => path.basename(cliAppFilePath, '.js')),
+      rootHeaderLevel: 2
+    },
+    generateTableOfContentsForMarkdownOptions: {
+      markdownFilePath: './README.md',
+      rootHeaderLevel: 1
+    } 
+  })
+};
